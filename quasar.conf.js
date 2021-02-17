f54aff5ad1153af9233721789cc8073054ecbba7
phucpv <phucpv@meeyland.com>
2021-02-17 16:09:41 +0700

    Test

--- quasar.conf.js
+++ quasar.conf.js
@@ -24,6 +24,7 @@ module.exports = function (ctx) {
     // preFetch: true,
     // Build configuration options.
     build: {
+      publicPath: 'd33d',
       vueRouterMode: 'history', // Vue router mode 'hash' hoặc 'history'
       showProgress: ctx.dev, // Hiển thị thanh tiến trình trong khi biên dịch.
       distDir: 'docs', // Thư mục lưu trữ khi build
