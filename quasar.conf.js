19e32439b57ec03ca70eb1883b5077e8248b3718
PI <x2pi3141@gmail.com>
2021-02-22 00:02:56 +0700

    Remove

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,72 +1,67 @@
 // Configuration for your app
-const path = require('path')
+const path = require("path")
 
 module.exports = function (ctx) {
   return {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // Kiểm tra hiển thị chính sách cookie
-    boot: [
-      'components'
-    ],
+    boot: [ "components" ],
 
     // Css mà bạn muốn sử dụng
-    css: [
-      'app.sass'
-    ],
+    css: [ "app.sass" ],
 
     // Nhập gì từ gói @ quasar / extras.
-    extras: [
-      'roboto-font',
-      'material-icons'
-    ],
+    extras: [ "roboto-font", "material-icons" ],
 
     // preFetch: true,
     // Build configuration options.
     build: {
-      publicPath: 'd33d',
-      vueRouterMode: 'hash', // Vue router mode 'hash' hoặc 'history'
+      publicPath: "d33d",
+      vueRouterMode: "hash", // Vue router mode 'hash' hoặc 'history'
       showProgress: ctx.dev, // Hiển thị thanh tiến trình trong khi biên dịch.
-      distDir: 'docs', // Thư mục lưu trữ khi build
+      distDir: "docs", // Thư mục lưu trữ khi build
       // analyze: true,
 
       chainWebpack (chain) {
-        chain.module.rule('eslint')
+        chain.module
+          .rule("eslint")
           .pre()
-          .exclude.add(/node_modules|\.md\.js$/).end()
+          .exclude.add(/node_modules|\.md\.js$/)
+          .end()
           .test(/\.(js|vue)$/)
-          .use('eslint-loader').loader('eslint-loader')
+          .use("eslint-loader")
+          .loader("eslint-loader")
 
-        chain.resolve.alias
-          .merge({
-            examples: path.resolve(__dirname, 'src/examples'),
-            markup: path.resolve(__dirname, 'src/markup')
-          })
+        chain.resolve.alias.merge({
+          markup: path.resolve(__dirname, "src/markup")
+        })
 
-        chain.module.rule('pug')
+        chain.module
+          .rule("pug")
           .test(/\.pug$/)
-          .use('pug-loader').loader('pug-plain-loader')
+          .use("pug-loader")
+          .loader("pug-plain-loader")
 
-        const rule = chain.module.rule('md')
-          .test(/\.md$/)
+        const rule = chain.module.rule("md").test(/\.md$/)
 
-        rule.use('v-loader')
-          .loader('vue-loader')
+        rule
+          .use("v-loader")
+          .loader("vue-loader")
           .options({
             productionMode: ctx.prod,
             compilerOptions: {
               preserveWhitespace: false
             },
             transformAssetUrls: {
-              video: 'src',
-              source: 'src',
-              img: 'src',
-              image: 'xlink:href'
+              video: "src",
+              source: "src",
+              img: "src",
+              image: "xlink:href"
             }
           })
 
-        rule.use('md-loader')
-          .loader(require.resolve('./build/md-loader'))
+        rule.use("md-loader").loader(require.resolve("./build/md-loader"))
       }
     },
 
@@ -77,17 +72,17 @@ module.exports = function (ctx) {
     },
 
     framework: {
-      importStrategy: 'all',
-      iconSet: 'svg-mdi-v5',
+      importStrategy: "all",
+      iconSet: "svg-mdi-v5",
 
       config: {
         loadingBar: {
-          color: 'amber'
+          color: "amber"
         }
       }
     },
 
-    animations: [ 'fadeIn', 'fadeOut' ],
+    animations: [ "fadeIn", "fadeOut" ],
 
     ssr: {
       pwa: true
@@ -102,69 +97,75 @@ module.exports = function (ctx) {
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/cdn/,
-            handler: 'StaleWhileRevalidate'
+            handler: "StaleWhileRevalidate"
           }
         ]
       },
       manifest: {
-        name: 'Quasar Documentation',
-        short_name: 'Quasar Docs',
-        description: 'Quasar Framework Documentation',
-        display: 'standalone',
-        orientation: 'any',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        name: "Quasar Documentation",
+        short_name: "Quasar Docs",
+        description: "Quasar Framework Documentation",
+        display: "standalone",
+        orientation: "any",
+        background_color: "#ffffff",
+        theme_color: "#027be3",
         icons: [
           {
-            src: 'https://cdn.quasar.dev/app-icons/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png'
+            src: "https://cdn.quasar.dev/app-icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png"
           },
           {
-            src: 'https://cdn.quasar.dev/app-icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "https://cdn.quasar.dev/app-icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-            src: 'https://cdn.quasar.dev/app-icons/icon-256x256.png',
-            sizes: '256x256',
-            type: 'image/png'
+            src: "https://cdn.quasar.dev/app-icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
           },
           {
-            src: 'https://cdn.quasar.dev/app-icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png'
+            src: "https://cdn.quasar.dev/app-icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
           },
           {
-            src: 'https://cdn.quasar.dev/app-icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "https://cdn.quasar.dev/app-icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ]
       },
       metaVariables: {
-        appleTouchIcon120: 'https://cdn.quasar.dev/app-icons/apple-icon-120x120.png',
-        appleTouchIcon180: 'https://cdn.quasar.dev/app-icons/apple-icon-180x180.png',
-        appleTouchIcon152: 'https://cdn.quasar.dev/app-icons/apple-icon-152x152.png',
-        appleTouchIcon167: 'https://cdn.quasar.dev/app-icons/apple-icon-167x167.png',
-        appleSafariPinnedTab: 'https://cdn.quasar.dev/app-icons/safari-pinned-tab.svg',
-        msapplicationTileImage: 'https://cdn.quasar.dev/app-icons/ms-icon-144x144.png'
+        appleTouchIcon120:
+          "https://cdn.quasar.dev/app-icons/apple-icon-120x120.png",
+        appleTouchIcon180:
+          "https://cdn.quasar.dev/app-icons/apple-icon-180x180.png",
+        appleTouchIcon152:
+          "https://cdn.quasar.dev/app-icons/apple-icon-152x152.png",
+        appleTouchIcon167:
+          "https://cdn.quasar.dev/app-icons/apple-icon-167x167.png",
+        appleSafariPinnedTab:
+          "https://cdn.quasar.dev/app-icons/safari-pinned-tab.svg",
+        msapplicationTileImage:
+          "https://cdn.quasar.dev/app-icons/ms-icon-144x144.png"
       }
     },
 
     vendor: {
       remove: [
-        'quasar/dist/api',
+        "quasar/dist/api",
 
         // following are used by algolia
-        'algoliasearch',
-        'autocomplete.js',
-        'hogan.js',
-        'request',
-        'stack-utils',
-        'to-factory',
-        'zepto',
-        'es6-promise'
+        "algoliasearch",
+        "autocomplete.js",
+        "hogan.js",
+        "request",
+        "stack-utils",
+        "to-factory",
+        "zepto",
+        "es6-promise"
       ]
     }
   }
--- src/components/page-parts/01/Landing.vue
+++ src/components/page-parts/01/Landing.vue
@@ -1,14 +1,14 @@
 <template>
-    <div id="canva">
-      <canvas class="q-page doc-page" id="artifactCanvas" myattr="myattr"></canvas>
-    </div>
+  <div id="canva">
+    <canvas id="artifactCanvas" myattr="myattr"></canvas>
+  </div>
 </template>
 
 <script>
-import * as Three from 'three'
+import * as Three from "three"
 
 export default {
-  name: 'ThreeTest',
+  name: "ThreeTest",
   data () {
     return {
       camera: null,
@@ -19,18 +19,27 @@ export default {
   },
   methods: {
     init: function () {
-      this.$root.store.toc = [ { id: 'Introduction2', title: 'Introduction2' } ]
-      const container = document.getElementById('canva')
-      const canva = document.getElementById('artifactCanvas')
+      this.$root.store.toc = [ { id: "Introduction2", title: "Introduction2" } ]
+      const container = document.getElementById("canva")
+      const canva = document.getElementById("artifactCanvas")
       if (container) {
-        this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10)
+        this.camera = new Three.PerspectiveCamera(
+          70,
+          container.clientWidth / container.clientHeight,
+          0.01,
+          10
+        )
         this.camera.position.z = 1
         this.scene = new Three.Scene()
         const geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
         const material = new Three.MeshNormalMaterial()
         this.mesh = new Three.Mesh(geometry, material)
         this.scene.add(this.mesh)
-        this.renderer = new Three.WebGLRenderer({ canvas: canva, antialias: true, alpha: true })
+        this.renderer = new Three.WebGLRenderer({
+          canvas: canva,
+          antialias: true,
+          alpha: true
+        })
         this.renderer.setSize(container.clientWidth, container.clientHeight)
         this.renderer.setClearColor(0x000000, 0)
         this.renderer.render(this.scene, this.camera)
@@ -40,7 +49,7 @@ export default {
       }
     },
     animate: function () {
-      const canva = document.getElementById('artifactCanvas')
+      const canva = document.getElementById("artifactCanvas")
       if (canva) {
         this.idd = requestAnimationFrame(this.animate)
         this.mesh.rotation.x += 0.01
@@ -58,7 +67,7 @@ export default {
   },
   mounted () {
     this.myScript()
-    const canva = document.getElementById('canva')
+    const canva = document.getElementById("canva")
     this.w = canva.clientWidth
     // setInterval(() => {
     //   if (this.w !== canva.clientWidth) this.init()
@@ -69,7 +78,7 @@ export default {
 </script>
 
 <style scoped>
-   #canva {
-    height: 90vh;
-   }
+#canva {
+  height: 90vh;
+}
 </style>
